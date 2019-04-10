992bc0ca03f3f184d96679d6e03e46b60555c285
LacourVincent <lacour.vincent.app@gmail.com>
2019-04-10 23:04:16 +0200

    Add GregoryLeibniz and Nilakantha method of pi calculation

--- src/core.py
+++ src/core.py
@@ -1,31 +1,56 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-N = 1000  # Number of points of the approximation
-R = 1  # Radius of the circle
 
-angles = np.linspace(0, 2 * np.pi, N+1)
-points = [(R*np.cos(angle), R*np.sin(angle))
-          for angle in angles]
+def getPiFromPolygonalApproximation(N, R):
+    angles = np.linspace(0, 2 * np.pi, N+1)
+    points = [(R*np.cos(angle), R*np.sin(angle))
+              for angle in angles]
+    distances = []
+    for i in range(len(points)-1):
+        p1 = points[i]
+        p2 = points[i + 1]
+        distance = np.hypot(p2[0] - p1[0], p2[1] - p1[1])
+        distances.append(distance)
+    return sum(distances) / (2 * R)
 
-distances = []
-for i in range(len(points)-1):
-    p1 = points[i]
-    p2 = points[i + 1]
-    distance = np.hypot(p2[0] - p1[0], p2[1] - p1[1])
-    distances.append(distance)
 
-pi = sum(distances) / (2 * R)
-print("Value of PI : {}".format(pi))
+def getPiFromGregoryLeibniz(N):
+    pi = 1
+    for i in range(1, N):
+        pi += ((-1)**i)*1/(i*2 + 1)
+    return 4*pi
 
-x = [point[0] for point in points]
-y = [point[1] for point in points]
 
-plt.plot(x, y, ".")
-plt.title(
-    "Approximation of a circle using polygon with N = {}".format(N), fontsize=15)
-plt.axis("equal")
-plt.xlabel("X")
-plt.ylabel("Y")
-plt.show()
-plt.close()
+def getPiFromNilakantha(N):
+    pi = 3
+    for i in range(1, N):
+        pi += ((-1)**(i-1))*4/((2*i*(2*i+1)*(2*i+2)))
+    return pi
+
+
+def plotFigure(points):
+    x = [point[0] for point in points]
+    y = [point[1] for point in points]
+    plt.plot(x, y, ".")
+    plt.title("Approximation of a circle using polygon with N = {}".format(
+        len(points) - 1), fontsize=15)
+    plt.axis("equal")
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.show()
+    plt.close()
+
+
+piFromPolygonalApproximation = getPiFromPolygonalApproximation(1000, 1)
+piFromGregoryLeibniz = getPiFromGregoryLeibniz(1000)
+piFromNilakantha = getPiFromNilakantha(1000)
+
+print("Polygonal Appproximation:")
+print("π = {}".format(piFromPolygonalApproximation))
+
+print("Gregory Leibniz Serie:")
+print("π = {}".format(piFromGregoryLeibniz))
+
+print("Nilakantha Serie:")
+print("π = {}".format(piFromNilakantha))
